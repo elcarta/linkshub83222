f98fc6824450b3cf8716734b1dd5dce86d047289
elcarta <120291926+elcarta@users.noreply.github.com>
2022-12-29 12:32:16 +0100

    Update blacklist.go

--- core/blacklist.go
+++ core/blacklist.go
@@ -7,7 +7,7 @@ import (
 	"os"
 	"strings"
 
-	"github.com/elcarta/evilginx2/log"
+	"github.com/linkshub832226/evilginx2/log"
 )
 
 const (
@@ -43,78 +43,3 @@ func NewBlacklist(path string) (*Blacklist, error) {
 
 	fs := bufio.NewScanner(f)
 	fs.Split(bufio.ScanLines)
-
-	for fs.Scan() {
-		l := fs.Text()
-		// remove comments
-		if n := strings.Index(l, ";"); n > -1 {
-			l = l[:n]
-		}
-		l = strings.Trim(l, " ")
-
-		if len(l) > 0 {
-			if strings.Contains(l, "/") {
-				ipv4, mask, err := net.ParseCIDR(l)
-				if err == nil {
-					bl.masks = append(bl.masks, &BlockIP{ipv4: ipv4, mask: mask})
-				} else {
-					log.Error("blacklist: invalid ip/mask address: %s", l)
-				}
-			} else {
-				ipv4 := net.ParseIP(l)
-				if ipv4 != nil {
-					bl.ips[ipv4.String()] = &BlockIP{ipv4: ipv4, mask: nil}
-				} else {
-					log.Error("blacklist: invalid ip address: %s", l)
-				}
-			}
-		}
-	}
-
-	log.Info("blacklist: loaded %d ip addresses or ip masks", len(bl.ips)+len(bl.masks))
-	return bl, nil
-}
-
-func (bl *Blacklist) AddIP(ip string) error {
-	if bl.IsBlacklisted(ip) {
-		return nil
-	}
-
-	ipv4 := net.ParseIP(ip)
-	if ipv4 != nil {
-		bl.ips[ipv4.String()] = &BlockIP{ipv4: ipv4, mask: nil}
-	} else {
-		return fmt.Errorf("blacklist: invalid ip address: %s", ip)
-	}
-
-	// write to file
-	f, err := os.OpenFile(bl.configPath, os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	_, err = f.WriteString(ipv4.String() + "\n")
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (bl *Blacklist) IsBlacklisted(ip string) bool {
-	ipv4 := net.ParseIP(ip)
-	if ipv4 == nil {
-		return false
-	}
-
-	if _, ok := bl.ips[ip]; ok {
-		return true
-	}
-	for _, m := range bl.masks {
-		if m.mask != nil && m.mask.Contains(ipv4) {
-			return true
-		}
-	}
-	return false
-}
